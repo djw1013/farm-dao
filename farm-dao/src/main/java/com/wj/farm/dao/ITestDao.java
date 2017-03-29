04c2ece2aebc8cff8f727d2326002d1ed6a334ce
16060834 <16060834@CNHQ-16060834N.sn.suning.ad>
2017-03-29 20:20:56 +0800

    djw初始化导入

---
+++ farm-dao/src/main/java/com/wj/farm/dao/ITestDao.java
@@ -0,0 +1,8 @@
+package com.wj.farm.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ITestDao {
+    List<Map<String, Object>> queryList();
+}
---
+++ farm-dao/src/main/java/com/wj/farm/dao/impl/TestDaoImpl.java
@@ -0,0 +1,26 @@
+package com.wj.farm.dao.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.wj.farm.core.util.xml.SqlUtil;
+import com.wj.farm.dao.ITestDao;
+
+@Repository
+public class TestDaoImpl implements ITestDao{
+
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+    
+    public List<Map<String, Object>> queryList() {
+        List<Map<String, Object>> list = jdbcTemplate.queryForList(SqlUtil.getSql("test", "selectSql"));
+        return list;
+    }
+
+
+}
